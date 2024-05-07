3d5214c2d764ef3c7c09dccb3de5d7ea5a76e5f2
Donsufia <udofia.donald@gmail.com>
2024-05-07 15:29:37 +0100

    Initial commit for this project

---
+++ script.js
@@ -0,0 +1,8 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Example of JavaScript interactivity
+    var navbarBrand = document.querySelector('.navbar-brand');
+    navbarBrand.addEventListener('click', function() {
+        alert('You clicked the navbar brand!');
+    });
+});
